306fe7e1d70aef9bdcef2af163d926daea134255
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-16 06:38:16 +0200

    Lecture 8 - Agent input

---
+++ _Scripts/AgentInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AgentInput : MonoBehaviour
+{
+    [field: SerializeField]
+    public UnityEvent<Vector2> OnMovementKeyPressed { get; set; }
+
+    private void Update()
+    {
+        GetMovementInput();
+    }
+
+    private void GetMovementInput()
+    {
+        OnMovementKeyPressed?.Invoke(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
+    }
+}
---
+++ _Scripts/AgentMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMovement : MonoBehaviour
+{
+    public void MoveAgent(Vector2 movementInput)
+    {
+        Debug.Log(movementInput);
+    }
+}
