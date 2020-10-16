b2b9c9675dbc86c16c10c37286edd877c09660aa
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-16 06:44:05 +0200

    Lecture 9 - Basic Player Movement

--- _Scripts/AgentMovement.cs
+++ _Scripts/AgentMovement.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class AgentMovement : MonoBehaviour
 {
+    protected Rigidbody2D rigidbody2d;
+
+    [SerializeField]
+    protected float currentVelocity = 3;
+
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
     public void MoveAgent(Vector2 movementInput)
     {
-        Debug.Log(movementInput);
+        rigidbody2d.velocity = movementInput.normalized * currentVelocity;
     }
 }
