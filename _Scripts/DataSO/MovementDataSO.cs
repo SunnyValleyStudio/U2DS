91002f0d64cab981f2616bc8aba13538cd45d5ba
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-16 06:59:22 +0200

    Lecture 10 Movement Data Scriptable Object

---
+++ _Scripts/DataSO/MovementDataSO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Agent/MovementData")]
+public class MovementDataSO : ScriptableObject
+{
+    [Range(1,10)]
+    public float maxSpeed = 5;
+
+    [Range(0.1f,100)]
+    public float aceleration = 50, deacceleration = 50;
+}
