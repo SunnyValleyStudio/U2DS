6e92b605c977cf143f434d263992d288ba25731b
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-16 10:09:07 +0200

    Lecture 15 - Rendering player

---
+++ _Scripts/AgentRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AgentRenderer : MonoBehaviour
+{
+    protected SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Debug.Log("Right cross product " + Vector3.Cross(Vector2.up, Vector2.right));
+        Debug.Log("Left cross product " + Vector3.Cross(Vector2.up, -Vector2.right));
+    }
+
+    public void FaceDirection(Vector2 pointerinput)
+    {
+        var direction = (Vector3)pointerinput - transform.position;
+        var result = Vector3.Cross(Vector2.up, direction);
+        if(result.z > 0)
+        {
+            spriteRenderer.flipX = true;
+        }else if(result.z < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
