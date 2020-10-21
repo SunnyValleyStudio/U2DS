3a7abd25424643d5b1045c94de0ef6beea90170d
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-21 12:15:12 +0200

    Lecture 28 - Aiming our weapon through script

---
+++ _Scripts/AgentWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentWeapon : MonoBehaviour
+{
+    protected float desiredAngle;
+
+    public virtual void AimWeapon(Vector2 pointerposition)
+    {
+        var aimDirection = (Vector3)pointerposition - transform.position;
+        desiredAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(desiredAngle, Vector3.forward);
+    }
+}
