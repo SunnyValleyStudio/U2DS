fd3cddf66c8975a56e6d27b33cc64a72f562a6ba
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-21 12:52:16 +0200

    Lecture 29 - Rendering the weapon correctly

--- _Scripts/AgentWeapon.cs
+++ _Scripts/AgentWeapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,26 @@ public class AgentWeapon : MonoBehaviour
 {
     protected float desiredAngle;
 
+    [SerializeField]
+    protected WeaponRenderer weaponRenderer;
+
+    private void Awake()
+    {
+        weaponRenderer = GetComponentInChildren<WeaponRenderer>();
+    }
+
     public virtual void AimWeapon(Vector2 pointerposition)
     {
         var aimDirection = (Vector3)pointerposition - transform.position;
         desiredAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        AdjustWeaponRendering();
         transform.rotation = Quaternion.AngleAxis(desiredAngle, Vector3.forward);
+    }
+
+    protected void AdjustWeaponRendering()
+    {
+
+        weaponRenderer?.FlipSprite(desiredAngle > 90 || desiredAngle < -90);
+        weaponRenderer?.RenderBehindHead(desiredAngle < 180 && desiredAngle > 0);
     }
 }
---
+++ _Scripts/Weapons/WeaponRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class WeaponRenderer : MonoBehaviour
+{
+    [SerializeField]
+    protected int playerSortingOrder = 0;
+    private SpriteRenderer weaponRenderer = null;
+
+    private void Awake()
+    {
+        weaponRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void FlipSprite(bool val)
+    {
+        weaponRenderer.flipY = val;
+    }
+
+    public void RenderBehindHead(bool val)
+    {
+        if (val)
+        {
+            weaponRenderer.sortingOrder = playerSortingOrder - 1;
+        }
+        else
+        {
+            weaponRenderer.sortingOrder = playerSortingOrder + 1;
+        }
+    }
+}
