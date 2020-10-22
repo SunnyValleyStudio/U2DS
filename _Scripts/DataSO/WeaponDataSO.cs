f9bf3e85995725416a7c839e836d9338e68e95b1
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-22 10:56:40 +0200

    Lecture 32 - Finishing Weapon script P1

---
+++ _Scripts/DataSO/WeaponDataSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapons/WeaponData")]
+public class WeaponDataSO : ScriptableObject
+{
+    public int AmmoCapacity { get; internal set; }
+}
--- _Scripts/Weapons/Weapon.cs
+++ _Scripts/Weapons/Weapon.cs
@@ -2,9 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField]
+    protected GameObject muzzle;
+
+    [SerializeField]
+    protected int ammo = 10;
+
+    [SerializeField]
+    protected WeaponDataSO weaponData;
+
+    public int Ammo
+    {
+        get { return ammo; }
+        set {
+            ammo = Mathf.Clamp(value, 0, weaponData.AmmoCapacity);
+        }
+    }
+
+    public bool AmmoFull { get => Ammo >= weaponData.AmmoCapacity; }
+
+    [field: SerializeField]
+    public UnityEvent OnShoot { get; set; }
+
+    [field: SerializeField]
+    public UnityEvent OnShootNoAmmo { get; set; }
+
+    private void Start()
+    {
+        Ammo = weaponData.AmmoCapacity;
+    }
+
     public void Shoot()
     {
         Debug.Log("Shooting");
