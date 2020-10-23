09cf49819ea1b1469264de29156dabc9218337d4
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-23 12:04:47 +0200

    Lecture 41 - Adding Weapon Audio

---
+++ _Scripts/AudioPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public abstract class AudioPlayer : MonoBehaviour
+{
+    protected AudioSource audioSource;
+    [SerializeField]
+    protected float pitchRandomness = 0.05f;
+    protected float basePitch;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        basePitch = audioSource.pitch;
+    }
+
+    protected void PlayClipWithVariablePitch(AudioClip clip)
+    {
+        var randomPitch = UnityEngine.Random.Range(-pitchRandomness, pitchRandomness);
+        audioSource.pitch = basePitch + randomPitch;
+        PlayClip(clip);
+    }
+
+    protected void PlayClip(AudioClip clip)
+    {
+        audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+}
---
+++ _Scripts/Weapons/WeaponAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAudio : AudioPlayer
+{
+    [SerializeField]
+    private AudioClip shootBulletClip = null, outOfBulletsClip = null;
+
+    public void PlayShootSound()
+    {
+        PlayClip(shootBulletClip);
+    }
+
+    public void PlayNoBulletsSound()
+    {
+        PlayClip(outOfBulletsClip);
+    }
+}
