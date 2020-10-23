302a5977d811a326ed754b1995ee30e419c9f66a
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-23 14:24:46 +0200

    Lecture 42 - Crosshair cursor icon

---
+++ _Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D cursorTexture = null;
+
+    private void Start()
+    {
+        SetCursorIcon();
+    }
+
+    private void SetCursorIcon()
+    {
+        Cursor.SetCursor(cursorTexture, new Vector2(cursorTexture.width / 2f, cursorTexture.height / 2f), CursorMode.Auto);
+    }
+}
