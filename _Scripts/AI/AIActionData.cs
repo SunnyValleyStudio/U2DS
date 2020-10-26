6f9300b12fa356d921bed24d9c2863ca72686635
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-26 09:18:03 +0100

    Lecture 50 - Sate Machine P1 (previous commit was lecture 49)

---
+++ _Scripts/AI/AIActionData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIActionData : MonoBehaviour
+{
+    [field: SerializeField]
+    public bool Attack { get; set; }
+    [field: SerializeField]
+    public bool TargetSpotted { get; set; }
+    [field: SerializeField]
+    public bool Arrived { get; set; }
+}
---
+++ _Scripts/AI/AIMovementData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovementData : MonoBehaviour
+{
+    [field: SerializeField]
+    public Vector2 Direction { get; set; }
+    [field: SerializeField]
+    public Vector2 PointOfInterest { get; set; }
+}
