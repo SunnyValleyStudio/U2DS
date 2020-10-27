6e0905c89c427bd9f124a785d48d085ead8cb9a7
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-27 10:42:38 +0100

    Lecture 54 - Chase and Idle action

---
+++ _Scripts/AI/Actions/ChaseAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseAction : AIAction
+{
+    public override void TakeAction()
+    {
+        var direction = enemyBrain.Target.transform.position - transform.position;
+        aiMovementData.Direction = direction.normalized;
+        aiMovementData.PointOfInterest = enemyBrain.Target.transform.position;
+        enemyBrain.Move(aiMovementData.Direction, aiMovementData.PointOfInterest);
+    }
+}
---
+++ _Scripts/AI/Actions/IdleAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleAction : AIAction
+{
+    public override void TakeAction()
+    {
+        aiMovementData.Direction = Vector2.zero;
+        aiMovementData.PointOfInterest = transform.position;
+        enemyBrain.Move(aiMovementData.Direction, aiMovementData.PointOfInterest);
+    }
+}
