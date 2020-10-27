bad9e2afcaac979b65b7239f024b0f6cac58c285
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-27 10:26:38 +0100

    Lecture 53 - Distance Decision

---
+++ _Scripts/AI/Decisions/DistanceDecision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDecision : AIDecision
+{
+    [field: SerializeField]
+    [field: Range(0.1f, 10)]
+    public float Distance { get; set; } = 5;
+
+    public override bool MakeADecision()
+    {
+        if(Vector3.Distance(enemyBrain.Target.transform.position, transform.position) < Distance)
+        {
+            if (aiActionData.TargetSpotted == false)
+            {
+                aiActionData.TargetSpotted = true;
+            } 
+        }
+        else
+        {
+            aiActionData.TargetSpotted = false;
+        }
+        return aiActionData.TargetSpotted;
+    }
+
+    protected void OnDrawGizmos()
+    {
+        if(UnityEditor.Selection.activeObject == gameObject)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, Distance);
+            Gizmos.color = Color.white;
+        }
+    }
+}
