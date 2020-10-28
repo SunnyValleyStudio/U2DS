f4fde6915313bb19651baddd4a9867256e339a1c
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-10-28 10:58:28 +0100

    Lecture 61 - Feedback architecture setup

---
+++ _Scripts/Feedback/Feedback.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Feedback : MonoBehaviour
+{
+    public abstract void CreateFeedback();
+    public abstract void CompletePreviousFeedback();
+}
---
+++ _Scripts/Feedback/FeedbackPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private List<Feedback> feedbackToPlay = null;
+
+    public void PlayFeedback()
+    {
+        FinishFeedback();
+        foreach (var feedback in feedbackToPlay)
+        {
+            feedback.CreateFeedback();
+        }
+    }
+
+    private void FinishFeedback()
+    {
+        foreach (var feedback in feedbackToPlay)
+        {
+            feedback.CompletePreviousFeedback();
+        }
+    }
+}
