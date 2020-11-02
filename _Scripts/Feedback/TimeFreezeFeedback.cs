a893770f817eb409cc99b341292e0621d5efe566
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-02 11:37:18 +0100

    Lecture 76 - Time freez feedback

---
+++ _Scripts/Feedback/TimeFreezeFeedback.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeFreezeFeedback : Feedback
+{
+    [SerializeField]
+    private float freezeTimeDelay = 0.01f, unfreezeTimeDelay = 0.02f;
+    [SerializeField]
+    [Range(0, 1)]
+    private float timeFreezeValue = 0.2f;
+
+    public override void CompletePreviousFeedback()
+    {
+        if (TimeController.instance != null)
+            TimeController.instance.ResetTimeScale();
+    }
+
+    public override void CreateFeedback()
+    {
+        TimeController.instance.ModifyTimeScale(timeFreezeValue, freezeTimeDelay, () => TimeController.instance.ModifyTimeScale(1, unfreezeTimeDelay));
+    }
+}
---
+++ _Scripts/TimeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public static TimeController instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }else if (instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    public void ResetTimeScale()
+    {
+        StopAllCoroutines();
+        Time.timeScale = 1;
+    }
+
+    public void ModifyTimeScale(float endTimeValue, float timeToWait, Action OnCompleteCallback = null)
+    {
+        StartCoroutine(TimeScaleCoroutine(endTimeValue, timeToWait, OnCompleteCallback));
+    }
+
+    IEnumerator TimeScaleCoroutine(float endTimeValue, float timeToWait, Action OnCompleteCallback)
+    {
+        yield return new WaitForSecondsRealtime(timeToWait);
+        Time.timeScale = endTimeValue;
+        OnCompleteCallback?.Invoke();
+    }
+
+}
