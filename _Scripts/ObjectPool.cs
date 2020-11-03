d7180a2140329c259df8d929220212fe87442093
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-03 11:22:40 +0100

    Lecture 80 - Bullet shell generator - object pooling

---
+++ _Scripts/ObjectPool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject objectToSpawn;
+    [SerializeField]
+    protected int poolSize;
+    protected int currentSize;
+    protected Queue<GameObject> objectPool;
+
+    private void Awake()
+    {
+        objectPool = new Queue<GameObject>();
+    }
+
+    public virtual GameObject SpawnObject(GameObject currentObject = null)
+    {
+        if (currentObject == null)
+            currentObject = objectToSpawn;
+
+        GameObject spawnedObject = null;
+
+        if(currentSize < poolSize)
+        {
+            spawnedObject = Instantiate(currentObject, transform.position, Quaternion.identity);
+            spawnedObject.name = currentObject.name + "_" + currentSize;
+            currentSize++;
+        }
+        else
+        {
+            spawnedObject = objectPool.Dequeue();
+            spawnedObject.transform.position = transform.position;
+            spawnedObject.transform.rotation = Quaternion.identity;
+        }
+        objectPool.Enqueue(spawnedObject);
+        return spawnedObject;
+    }
+}
