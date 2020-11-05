67a487917a4a0b705c76fdf24e160ad046570fdb
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-05 09:39:45 +0100

    Lecture 88 - Resteart button

--- _Scripts/GameManager.cs
+++ _Scripts/GameManager.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,5 +18,11 @@ public class GameManager : MonoBehaviour
     private void SetCursorIcon()
     {
         Cursor.SetCursor(cursorTexture, new Vector2(cursorTexture.width / 2f, cursorTexture.height / 2f), CursorMode.Auto);
+    }
+
+    public void RestartLevel()
+    {
+        DOTween.KillAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
