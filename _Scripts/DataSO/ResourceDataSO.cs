56c248b5b26a8c091b27189c4ad222bfc4d9dbf1
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-06 09:15:45 +0100

    Lecture 91 - ResourceDataSO and Resource script

---
+++ _Scripts/DataSO/ResourceDataSO.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Items/ResourceData")]
+public class ResourceDataSO : ScriptableObject
+{
+    [field: SerializeField]
+    public ResourceTypeEnum ResourceType { get; set; }
+    [SerializeField]
+    private int minAmount = 1, maxAmount = 5;
+
+    public int GetAmount()
+    {
+        return Random.Range(minAmount, maxAmount + 1);
+    }
+}
+
+public enum ResourceTypeEnum
+{
+    None,
+    Health,
+    Ammo
+}
---
+++ _Scripts/Resources/Resource.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Resource : MonoBehaviour
+{
+    [field: SerializeField]
+    public ResourceDataSO ResourceData { get; set; }
+
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PickUpResource()
+    {
+        StartCoroutine(DestroyCoroutine());
+    }
+
+    IEnumerator DestroyCoroutine()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+        Destroy(gameObject);
+    }
+}
